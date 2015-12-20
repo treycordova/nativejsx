060825513807c7037fbefaa730997d5b7b48f72b
Trey Cordova <trey.cordova@socrata.com>
2015-12-19 16:41:26 -0800

    Add example usage file;

---
+++ examples.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var jsxdom = require('./source/jsxdom');
+
+// Asynchronous, ideal for bulk operations.
+jsxdom.parse('test.jsx').then(function(byPromise) {
+  // ==>
+  console.log(byPromise);
+});
+
+// OR Synchronous, for callback and promise naysayers :).
+let bySync = jsxdom.parseSync('test.jsx');
+// ==>
+console.log(bySync);
+
+// OR We must go deeper.
+let byString = jsxdom.transpile('var something = <div></div>;');
+// ==>
+console.log(byString);
