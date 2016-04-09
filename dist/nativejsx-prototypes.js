d64844c3a84915df930fb1411d7f81e6e5c76aa4
Trey Cordova <trey.cordova@socrata.com>
2016-04-09 05:15:46 -0700

    Updates appendChildren and redists;

--- dist/nativejsx-prototypes.js
+++ dist/nativejsx-prototypes.js
@@ -103,7 +103,7 @@
 	  children.forEach(function(child) {
 	    if (child instanceof HTMLElement) {
 	      element.appendChild(child);
-	    } else if (child) {
+	    } else if (child || typeof child === 'string') {
 	      element.appendChild(document.createTextNode(child.toString()));
 	    } else {
 	      throw new DOMException('Failed to execute \'appendChildren\' on \'Element\': ' + Object.prototype.toString.call(child) + ' is not valid.');
