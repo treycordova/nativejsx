8175d01c4ba395797ca5753bbb8a0ee565d49d03
Trey Cordova <trey.cordova@socrata.com>
2017-02-26 21:21:19 -0800

    Adds a .catch to the promise example.

--- examples.js
+++ examples.js
@@ -1,10 +1,14 @@
 const nativejsx = require('./source/nativejsx');
 
 // Asynchronous, ideal for bulk operations.
-nativejsx.parse('./test/jsx/test.jsx').then((byPromise) => {
-  // ==>
-  console.log(byPromise);
-});
+nativejsx.parse('./test/jsx/test.jsx').
+  then((byPromise) => {
+    // ==>
+    console.log(byPromise);
+  }).
+  catch((error) => {
+    console.error(error);
+  });
 
 // OR Synchronous, for callback and promise naysayers :).
 const bySync = nativejsx.parseSync('./test/jsx/node-expressions.jsx', {prototypes: 'inline'});
